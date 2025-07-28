c671a2dd1552240ecedaeaab1c08856b4fbf3ce3
rohitshettigar <rohitshettigar103@gmail.com>
2025-07-28 12:57:16 +0530

    Update decoder.sv

--- decoder.sv
+++ decoder.sv
@@ -1,1 +1,17 @@
-
+module decoder ( input clk , reset , input [1:0] x , output reg
+[3:0] y);
+always @( posedge clk , posedge reset ) begin
+if ( reset )
+    y <=0;
+else
+begin
+case (x)
+3’ b11 :y <=4 ’ b1000 ;
+3’ b10 :y <=4 ’ b0100 ;
+3’ b01 :y <=4 ’ b0010 ;
+3’ b00 :y <=4 ’ b0001 ;
+default :y <=0;
+endcase
+end
+end
+endmodule
