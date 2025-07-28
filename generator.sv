738455b9eedd45bf398bc9efc67339d7cf2593b1
rohitshettigar <rohitshettigar103@gmail.com>
2025-07-28 13:02:15 +0530

    Update generator.sv

--- generator.sv
+++ generator.sv
@@ -1,1 +1,31 @@
+class generator ;
 
+int count , i;
+
+mailbox gen_to_drv ;
+
+transaction tr ;
+
+function new ( mailbox gen_to_drv );
+
+this . gen_to_drv = gen_to_drv ;
+
+endfunction
+
+task run ;
+
+for ( i =0; i < count +4; i =i +1)
+
+begin
+
+tr = new () ;
+
+{ tr . in }= i ;
+
+gen_to_drv . put ( tr );
+
+end
+
+endtask
+
+endclass
