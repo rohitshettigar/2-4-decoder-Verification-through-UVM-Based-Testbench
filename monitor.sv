487689576714ef7548c71b83937a76ad9e1fdd80
rohitshettigar <rohitshettigar103@gmail.com>
2025-07-28 13:01:00 +0530

    Update monitor.sv

--- monitor.sv
+++ monitor.sv
@@ -1,1 +1,41 @@
+class monitor ;
 
+virtual deco_if dif ;
+
+mailbox mon_to_sb ;
+
+function new ( mailbox mon_to_sb , virtual deco_if dif );
+
+this . dif = dif ;
+
+this . mon_to_sb = mon_to_sb ;
+
+endfunction
+
+task run ;
+
+forever begin
+
+transaction mon_tr ;
+
+wait (! dif . reset );
+
+@( posedge dif . clk );
+
+mon_tr = new () ;
+
+mon_tr . in = dif . in ;
+
+// mon_tr . en = dif . en ;
+
+@( posedge dif . clk );
+
+mon_tr . out = dif . out ;
+
+mon_to_sb . put ( mon_tr );
+
+end
+
+endtask
+
+endclassclass monitor ;
